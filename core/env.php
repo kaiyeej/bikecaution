12dd2a4a11cb415df2bdb4c640301790f7a18868
kaiyeej <58097285+kaiyeej@users.noreply.github.com>
2022-11-24 10:06:45 +0800

    db

--- core/env.php
+++ core/env.php
@@ -3,11 +3,14 @@
 define('BASE_URL', 'http://localhost/bike/');
 define("BASE_PATH", __DIR__ . "/../");
 
-define("HOST", "localhost");
+/*define("HOST", "localhost");
 define("USER", "root");
 define("PASSWORD", "");
-define("DBNAME", "bikecaution_db");
+define("DBNAME", "bikecaution_db");*/
 
-
+define("HOST", "localhost");
+define("USER", "u981310152_bikecaution_ro");
+define("PASSWORD", "v[EJb+s6R");
+define("DBNAME", "u981310152_bikecaution_db");
 
 ?>
